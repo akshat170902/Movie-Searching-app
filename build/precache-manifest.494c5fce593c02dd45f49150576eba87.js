3f2307001ea7e5e16fc1b8b97af3cec7755b9a32
akshat170902 <akshat.ug20@nsut.ac.in>
2022-07-02 02:03:23 +0530

    build folder

---
+++ build/precache-manifest.494c5fce593c02dd45f49150576eba87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b869f538dcb86bfd4f6f190a99b14171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db571b91e3c48fbb4879",
+    "url": "/static/css/main.f74c0547.chunk.css"
+  },
+  {
+    "revision": "b966fca9f8354845e981",
+    "url": "/static/js/2.74a105c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.74a105c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db571b91e3c48fbb4879",
+    "url": "/static/js/main.65c2fd2e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "b78f177a4111016eb97377a0bbd7444f",
+    "url": "/static/media/logo.b78f177a.png"
+  }
+]);
